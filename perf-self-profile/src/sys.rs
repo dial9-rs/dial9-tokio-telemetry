22b7818ddcb570c91f79de560974fb8e4cea9800
Julian <julian@wyeworks.com>
2026-04-10 18:01:54 +0200

    Replace glob re-exports with named imports

--- perf-self-profile/src/sys.rs
+++ perf-self-profile/src/sys.rs
@@ -1,9 +1,13 @@
 #[cfg(target_os = "linux")]
 mod linux;
 #[cfg(target_os = "linux")]
-pub use linux::*;
+pub(crate) use linux::write_symbol_data;
+#[cfg(target_os = "linux")]
+pub use linux::{PerfSampler, resolve_symbol, resolve_symbol_with_maps, resolve_symbols_with_maps};
 
 #[cfg(not(target_os = "linux"))]
 mod unsupported;
 #[cfg(not(target_os = "linux"))]
-pub use unsupported::*;
+pub(crate) use unsupported::write_symbol_data;
+#[cfg(not(target_os = "linux"))]
+pub use unsupported::{PerfSampler, resolve_symbol};
